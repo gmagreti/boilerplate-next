0d9e2abd419ba6746ffbd8d8f8c10cf8d851d0be
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-05-12 22:57:25 -0300

    chore: initial commit.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to react
         </h1>
 
         <p className={styles.description}>
